d7f3e24f6ecaff86a35d01ee634284290188ad09
himanshujainsanghai <himanshujainhj70662@gmail.com>
2025-05-13 23:21:35 +0530

    updated coupncontroller

--- models/couponModel.js
+++ models/couponModel.js
@@ -1,97 +1,110 @@
 const mongoose = require("mongoose");
 
-const couponSchema = new mongoose.Schema({
+const couponSchema = new mongoose.Schema(
+  {
     offerName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     couponCode: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     couponType: {
-        type: String,
-        enum: ['Public', 'Private'],
-        required: true
+      type: String,
+      enum: ["Public", "Private"],
+      required: true,
     },
-    assignedUsers: [{
+    assignedUsers: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }],
+        ref: "User",
+      },
+    ],
     courseSelectionType: {
-        type: String,
-        enum: ['All', 'Specific'],
-        required: true
+      type: String,
+      enum: ["All", "Specific"],
+      required: true,
     },
-    assignedCourses: [{
+    assignedCourses: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Course'
-    }],
+        ref: "Course",
+      },
+    ],
     usageLimit: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     isUnlimited: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     usagePerStudent: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     visibility: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
     discountType: {
-        type: String,
-        enum: ['Flat', 'Percentage'],
-        required: true
+      type: String,
+      enum: ["Flat", "Percentage"],
+      required: true,
     },
     discountAmount: {
-        type: Number,
-        required: true
+      type: Number,
+      default: 0,
+    },
+    discountPercentage: {
+      type: Number,
+      default: 0,
     },
     startDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     startTime: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     endDate: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     endTime: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isLifetime: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     minimumOrderValue: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     usageCount: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-    userUsage: [{
+    userUsage: [
+      {
         userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
         },
         usageCount: {
-            type: Number,
-            default: 0
-        }
-    }]
-}, { timestamps: true });
+          type: Number,
+          default: 0,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Coupon = mongoose.model("Coupon", couponSchema);
 
